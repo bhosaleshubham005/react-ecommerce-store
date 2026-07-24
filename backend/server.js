44b8a83a13b2869183ba3933df80c501f647b008
bhosaleshubham005 <bhosaleshubham575@gmail.com>
2026-07-24 13:52:03 +0530

    Setup Express server, MongoDB connection, Product model, and GET products route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const productRoutes = require("./routes/productRoutes");
+
+dotenv.config();
+
+connectDB();
+
 const app = express();
-const PORT = 5000 ;
 
-app.get("/",(req, res) =>{
-    res.send("welcome");
-})
+app.use(express.json());
 
+app.use(productRoutes);
 
-app.listen(PORT,() =>{
-    console.log("server is running at 5000")
-})
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend Connected");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
